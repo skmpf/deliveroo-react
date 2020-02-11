3ea034f0633b85766547d639bc632855931b73b8
Sebastien KEMPF <kemfou@thegrid.lan>
2020-02-11 14:57:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./components/css/reset.css";
+import "./App.css";
+
+import Header from "./components/Header";
+import Content from "./components/Content";
 
 function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://deliveroo-backend-2003.herokuapp.com/"
+      );
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading === true ? (
+        <div>Loading page</div>
+      ) : (
+        <>
+          <Header data={data} />
+          <Content categories={data.categories} />
+        </>
+      )}
+    </>
   );
 }
 
